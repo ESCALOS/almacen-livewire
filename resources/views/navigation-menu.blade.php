84d93f3549853517bc61007d28182390f9ba8c36
Escalos <stornblood6969@gmail.com>
2023-06-06 13:56:25 -0500

    Terminado de configurar plantilla de jetstream con wire ui

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -92,34 +92,17 @@
                             @endif
                         </x-slot>
 
-                        <x-slot name="content">
-                            <!-- Account Management -->
-                            <div class="block px-4 py-2 text-xs text-gray-400">
-                                {{ __('Manage Account') }}
-                            </div>
-
-                            <x-dropdown-link href="{{ route('profile.show') }}">
-                                {{ __('Profile') }}
-                            </x-dropdown-link>
-
-                            @if (Laravel\Jetstream\Jetstream::hasApiFeatures())
-                                <x-dropdown-link href="{{ route('api-tokens.index') }}">
-                                    {{ __('API Tokens') }}
-                                </x-dropdown-link>
-                            @endif
-
-                            <div class="border-t border-gray-200 dark:border-gray-600"></div>
-
-                            <!-- Authentication -->
+                        <x-dropdown.item icon="user-circle" label="{{ __('Profile') }}"  href="{{ route('profile.show') }}"/>
+                        <x-dropdown.item icon="arrow-circle-left" separator>
                             <form method="POST" action="{{ route('logout') }}" x-data>
                                 @csrf
 
-                                <x-dropdown-link href="{{ route('logout') }}"
-                                         @click.prevent="$root.submit();">
+                                <p href="{{ route('logout') }}"
+                                        @click.prevent="$root.submit();">
                                     {{ __('Log Out') }}
-                                </x-dropdown-link>
+                                </p>
                             </form>
-                        </x-slot>
+                        </x-dropdown.item>
                     </x-dropdown>
                 </div>
             </div>
